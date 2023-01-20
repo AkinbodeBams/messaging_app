cacf9200c49218892009239d3293d392d6c713b1
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-01-20 10:16:34 +0100

    changes

---
+++ messages.py
@@ -0,0 +1,55 @@
+messages = {
+    'b_day_msg': [f'The Lord your God is in your midst, a mighty one who will save; He will rejoice over you with '
+                  'gladness; he will quiet you by his love; he will exult over you with loud singing.” – Zephaniah 3:17 '
+                  'Happiest of birthdays to you, ###. May God hold His comforting hand over your life in the coming '
+                  'year',
+                  'May Jesus Christ continue to bless you abundantly and keep you safe in his loving care. On your '
+                  "birthday, enjoy His divine presence in your life. Have a happy and holy birthday, ###. You mean"
+                  " the world to us! Araba Baptist Church",
+                  "Life by itself is a gift, so never forget to thank God for it. Moreover, never forget to make the most"
+                  "of it. May God bless you on your birthday with abundant peace and joy. Happy birthday, ###. we "
+                  "love you more than words can say. Araba Baptist Church",
+                  "On your birthday, we wish you every ounce of happiness ###. We hope you bring glory to Jesus’s "
+                  "name every day of the coming year. Have fun and a blessed day. we love you. Araba Baptist Church",
+                  "God is always there to guide you, protect you and comfort you. You are never alone.He is always near. "
+                  "May you have a lovely day and an even more beautiful year ahead. Happy Birthday. May God bless you "
+                  "abundantly, ###! Araba Baptist Church",
+                  "Wishing you a day filled with happy memories and a year with numerous reasons to be thankful about. "
+                  "Happy Birthday ###. May God bless you. Araba Baptist Church",
+                  "Happy birthday, ###. On your birthday we pray to God for your health and well-being. May Jesus gift you "
+                  "all the success, pride, and prosperity. Have a great year ahead. Araba Baptist Church"],
+    'wed_msg': ["Love that grows through time is such an inspiring thing. Congratulations on your ### years together."
+                "Araba Baptist Church",
+                "Having each other’s backs and taking care of one another’s souls for ### years, that’s a beautiful faith walk."
+                "Happy Anniversary Araba Baptist Church",
+                "Sharing in your happiness as you celebrate ### years of marriage. Today is your day to be in the "
+                "spotlight…to celebrate all you’ve accomplished together…and to bask in all the admiration. "
+                "Araba Baptist Church",
+                "### years of marriage! It couldn’t have been easy every day, but you two make it look that way "
+                "Araba Baptist Church."
+
+                ],
+    'bday_months': [{1: '',
+                     2: "The month of February brings many gifts, one of those was you. A beautiful gift that "
+                     "captivated our soul and changed our lives. A lovely gift that we are grateful for. Welcome to your "
+                     "Birthday month. Araba Baptist Church",
+                     3: "The month of March brings many gifts, one of those was you. A beautiful gift that "
+                     "captivated our soul and changed our lives. A lovely gift that we are grateful for. Welcome to your "
+                     "Birthday month. Araba Baptist Church",
+                     4: "The month of April brings many gifts, one of those was you. A beautiful gift that "
+                     "captivated our soul and changed our lives. A lovely gift that we are grateful for. Welcome to your "
+                     "Birthday month. Araba Baptist Church"
+
+                     }],
+    'General_month': [{1: '',
+                       2: "In this new month, may God give you new hope and a fresh start\
+                as we start this month. May God arrange your steps on the right way, and may \
+                your day, and all the week in this month be full of bliss.Happy \
+                new Month to you and yours.",
+                       3: "You will shine like the sun and be precious as the diamonds. No matter what happens, you will always be at the top. We wish you a month that will shine as you shine. It is a month of possibilities. Enjoy the new month.",
+                       4: "In this new month, may you always testify as you live, and may you receive more than a downpour of rain of blessings? Happy new month.Araba Baptist Church"
+
+                       }
+
+                      ]
+}
--- sms.py
+++ sms.py
@@ -1,16 +1,15 @@
 import os
 import requests
-
-
-
+from dotenv import load_dotenv
 
 
 class Sms():
 
     def __init__(self):
-        self.url_for_get = f'https://portal.nigeriabulksms.com/api/?username={os.environ.get("sms_username")}&password={os.environ.get("sms_password")}&action'
-        self.url_for_post  = f'https://portal.nigeriabulksms.com/api/?username={os.environ.get("sms_username")}&password={os.environ.get("sms_password")}'
- 
+        load_dotenv()
+        self.url_for_get = f'https://portal.nigeriabulksms.com/api/?username={os.getenv("sms_username")}&password={os.getenv("sms_password")}&action'
+        self.url_for_post  = f'https://portal.nigeriabulksms.com/api/?username={os.getenv("sms_username")}&password={os.getenv("sms_password")}'
+    
     def balance_getter(self):
         url = f'{self.url_for_get}=balance'
         balance = requests.get(url=url).json()
@@ -25,6 +24,4 @@ class Sms():
         msg_number =f'{self.url_for_post}&message={message}&sender={sender}&mobiles={phone_numbers}'
         send_sms = requests.post(msg_number)
         return send_sms
-new = Sms()
 
-# print(new.report_getter())
